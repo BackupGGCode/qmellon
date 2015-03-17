c7fe2c0de5e31f904282d34db2e62eec28ba038b
Q Mellon <qmellon@gmail.com>
2015-03-18 01:00:48 +0300

    Fixed bin/bitch.sh Fixed usr/share/applications/Bitcheese.desktop

--- bin/bitch.sh
+++ bin/bitch.sh
@@ -2,23 +2,27 @@
 
 usage()
 {
-	echo "Usage: `basename $0` -[f|i|t][k][x][l language] <file>"
+	echo "Usage: `basename $0` -[f|i|t][d][k][x][l language] <file>"
 	echo
 	echo "	Object types:"
 	echo "		f	general file"
 	echo "		i	image file"
 	echo "		t	text file"
 	echo "	Other options:"
+	echo "		d	decode URL"
 	echo "		k	use kdialog"
 	echo "		x	xclip URL to clipboard"
 	echo "		l	set language type for colorize"
 }
 
-while getopts ":fitkxl:" OPTION
+while getopts ":fitdkxl:" OPTION
 do
 	case ${OPTION} in
 		"f" | "i" | "t" )
 			export OBJECT_TYPE="$OPTION"
+		;;
+		"d" )
+			export DECODE=1
 		;;
 		"k" )
 			which kdialog &> /dev/null && export K=1
@@ -38,24 +42,29 @@ do
 	shift $(($OPTIND - 1))
 done
 
+output() {
+	[[ "x$DECODE" == "x1" ]] && URL="$(echo -en ${URL//%/\\x})"
+	[[ "x$XCLIP" == "x1" ]] && echo -n "$URL" | nohup xclip -selection clipboard &> /dev/null &
+	if [[ "x$OBJECT_TYPE" == "xt" ]]; then
+		OUT="$URL"
+	else
+		OUT="$URL [$URL/preview]"
+	fi
+	if [[ "x$K" == "x1" ]]; then
+		kdialog --msgbox "$OUT"
+	else
+		echo "$OUT"
+	fi
+}
+
 a_file() {
 	URL=$(curl -s -w %{redirect_url} -F "file=@$1" http://dump.bitcheese.net/upload-file?simple)
-	[[ "x$XCLIP" == "x1" ]] && echo -n "$URL" | nohup xclip -selection clipboard &> /dev/null &
-	if [[ "x$K" == "x1" ]]; then
-		kdialog --msgbox "$URL [$URL/preview]"
-	else
-		echo "$URL [$URL/preview]"
-	fi
+	output
 }
 
 a_image() {
 	URL=$(curl -s -w %{redirect_url} -F "file=@$1" http://dump.bitcheese.net/upload-image?simple)
-	[[ "x$XCLIP" == "x1" ]] && echo -n "$URL" | nohup xclip -selection clipboard &> /dev/null &
-	if [[ "x$K" == "x1" ]]; then
-		kdialog --msgbox "$URL [$URL/preview]"
-	else
-		echo "$URL [$URL/preview]"
-	fi
+	output
 }
 
 a_text() {
@@ -65,42 +74,36 @@ a_text() {
 		PLANG="$p_lang"
 	fi
 	URL=$(curl -s -w %{redirect_url} -F "lang=$PLANG" -F "text=@$1" http://dump.bitcheese.net/upload-text)
-	[[ "x$XCLIP" == "x1" ]] && echo -n "$URL" | nohup xclip -selection clipboard &> /dev/null &
-	if [[ "x$K" == "x1" ]]; then
-		kdialog --msgbox "$URL [$URL/preview]"
-	else
-		echo "$URL"
-	fi
+	output
 }
 
 
 if [ -f "$1" ]; then
-	if [ -n "$OBJECT_TYPE" ]; then
-		case "$OBJECT_TYPE" in
-			"f")
-				a_file "$1"
-			;;
-			"i")
-				a_image "$1"
-			;;
-			"t")
-				a_text "$1"
-			;;
-				*) echo ":("
-			;;
-		esac
-	else
+	if [ -z "$OBJECT_TYPE" ]; then
 		case $(file -b --mime-type "$1") in
 			image/* )
-				a_image "$1"
+				export OBJECT_TYPE="i"
 			;;
 			text/* )
-				a_text "$1"
+				export OBJECT_TYPE="t"
 			;;
 			* )
-				a_file "$1"
+				export OBJECT_TYPE="f"
 		esac
 	fi
+	case "$OBJECT_TYPE" in
+		"f")
+			a_file "$1"
+		;;
+		"i")
+			a_image "$1"
+		;;
+		"t")
+			a_text "$1"
+		;;
+			*) echo ":("
+		;;
+	esac
 else
 	echo "$1 doesn't exist or isn't a regular file"
 	exit 1
